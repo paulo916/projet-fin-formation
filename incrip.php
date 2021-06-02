68fa60317128fbeccbfe6a5b62a2a4b1290dd904
paulo <paulo_magalhaes@outlook.fr>
2021-06-02 22:04:27 +0200

    end

--- incrip.php
+++ incrip.php
@@ -20,7 +20,6 @@ include_once('inc/dbconnect.inc.php');
     <title><?php echo APP_NAME; ?></title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 </head>
-
 <body class="container">
     <div class="jumbotron mt-5">
         <p>Vous pouvez vous inscrire où vous connecté en cliquant sur le bouton ci-dessous.</p>
