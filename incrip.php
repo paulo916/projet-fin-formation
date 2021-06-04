6624626f4531193f5cb9e84dd84962ce0fad846d
paulo <paulo_magalhaes@outlook.fr>
2021-06-04 13:09:38 +0200

    end

--- incrip.php
+++ incrip.php
@@ -18,9 +18,11 @@ include_once('inc/dbconnect.inc.php');
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo APP_NAME; ?></title>
+    <link rel="stylesheet" href="css/incrip.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 </head>
 <body class="container">
+    <section>
     <div class="jumbotron mt-5">
         <p>Vous pouvez vous inscrire où vous connecté en cliquant sur le bouton ci-dessous.</p>
 
@@ -29,6 +31,7 @@ include_once('inc/dbconnect.inc.php');
         <a href="connection.html"><button type="button" class="btn btn-success" data-toggle="modal" data-target="#login">Connexion</button></a>
         </span>
     </div>
+    </section>
     <?php
     include_once('inc/functions.inc.php');
     echo display_alert('c', 'ok', 'success', 'Bienvenue ' . (isset($_SESSION['pseudo']) ? $_SESSION['pseudo'] : '') . ' !');
@@ -38,5 +41,15 @@ include_once('inc/dbconnect.inc.php');
     echo display_alert('u', 'ok', 'success', 'Le compte a été créé avec succès !');
     echo display_alert('u', 'ko', 'danger', 'Le compte n\'a pas pu être créé !');
     ?>
+    <div id="conf">
+            <a href="mentions.html" target="_blank">Mentions légales et de confidenlité</a>
+    </div>
+    <footer id="rodape">
+
+        <p>Copyright &copy;2021 - by Paulo Magalhaes<br>
+            <a href="http://facebook.com" target="_blank">Facebook</a> |
+            <a href="http://twitter.com" target="_blank">Twitter</a>
+        </p>
+    </footer>
 </body>
 </html>
